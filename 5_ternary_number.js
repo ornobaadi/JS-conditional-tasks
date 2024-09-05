18d76821e2355cf9fd626c055342dc189ed52299
ornobaadi <aadi4789@gmail.com>
2024-09-05 09:00:38 +0600

    Ternary

--- 5_ternary_number.js
+++ 5_ternary_number.js
@@ -10,3 +10,21 @@ write a simple if-else.
 also, write it using ternary operator.
 
  */
+
+var num1 = 25;
+var num2 = 50;
+var result;
+
+if(num1 > num2){
+    result = num1 * 2;
+}
+else{
+    result = num1 + num2;
+}
+
+console.log(result);
+
+// TERNARY OPERATOR 
+
+var result = (num1 > num2) ? (num1 * 2) : (num1 + num2);
+console.log(result);
