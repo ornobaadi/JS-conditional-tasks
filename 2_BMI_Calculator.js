b12834e0b726b7ca3ab75927dc227ccea5dd7bb6
ornobaadi <aadi4789@gmail.com>
2024-09-05 08:31:10 +0600

    BMI

--- 2_BMI_Calculator.js
+++ 2_BMI_Calculator.js
@@ -11,3 +11,23 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
     - Otherwise, you are obese.
 
 */
+
+
+
+let weight = 80;   // KG
+let height = 1.7; // METER
+
+let BMI = weight / (height * height);
+
+if(BMI < 18){
+    console.log('You are underweight');
+}
+else if(BMI >= 18.5 && BMI <= 24.9){
+    console.log('You are normal');
+}
+else if(BMI >=25 && BMI <= 29.9){
+    console.log('You are overweight')
+}
+else{
+    console.log('You are obese')
+}
