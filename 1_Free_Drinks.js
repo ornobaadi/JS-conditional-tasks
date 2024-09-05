5ce4cb211a32c97e0a5ace37167228a00b42b76f
ornobaadi <aadi4789@gmail.com>
2024-09-05 08:11:25 +0600

    first commit

---
+++ 1_Free_Drinks.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burger = 200;
+
+if(burger > 500){
+    console.log('Congratulations! You get a free Coke');
+}
+else{
+    console.log('Sorry! Coke is 30Tk');
+}
